0f10716b950e947257a47bf8f118f385500d0d99
Rompo80 <romanworld@gmail.com>
2022-05-02 09:54:56 -0400

    button crée

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('#button').addEventListener('click', () => {
+    console.log('Bonjour!');
+    console.warn('Attention il\'y a une alerte!');
+    console.error(`Desolé mauvaise solutione!`);
+  });
+  
+  
+  
